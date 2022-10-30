1b5c018905d8367331d728650c8b81535bfad24d
Karim Kaylani <karimkaylani@gmail.com>
2022-10-30 11:20:26 -0700

    Ensured unique IDs, converted link text to 'Source'

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ JSON_FILE = 'data.json'
 def replace_url_to_link(value):
     # Replace url to link
     urls = re.compile(r"((https?):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)", re.MULTILINE|re.UNICODE)
-    value = urls.sub(r'<a href="\1" target="_blank">\1</a>', value)
+    value = urls.sub(r'<a href="\1" target="_blank">[Source]</a>', value)
     # Replace email to mailto
     urls = re.compile(r"([\w\-\.]+@(\w[\w\-]+\.)+[\w\-]+)", re.MULTILINE|re.UNICODE)
     value = urls.sub(r'<a href="mailto:\1">\1</a>', value)
